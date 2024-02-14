503279410bf33ba5d5a7e5d9f88eb283f2a55740
Ken <Ken>
2024-02-14 15:43:11 +0800

    Scrapper finished, bug fixing

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import csv
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
+import csv
 
-driver = webdriver.Chrome(executable_path='/path/to/chromedriver')
-
+driver = webdriver.Firefox()
 driver.get('https://www.hkex.com.hk/Market-Data/Securities-Prices/Equities?sc_lang=en')
+wait = WebDriverWait(driver, 10)
 
+wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'table.table_equities')))
 
-time.sleep(5)
+rows = driver.find_elements(By.CSS_SELECTOR, 'table.table_equities tbody tr.datarow')
 
-# find the elements containing stock codes and names and update the selectors based on the actual page structure
-stocks = driver.find_elements(By.CSS_SELECTOR, 'css_selector_for_stocks')
+# Iterating the "Load More" button
+while True:
+    try:
+        load_more_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.loadmore .load')))
+        driver.execute_script("arguments[0].click();", load_more_button)
+        time.sleep(0.2)
+    except Exception as e:
+        print("All data loaded or an error occurred:", e)
+        break
 
-# extract stock codes and name
 stock_data = []
-for stock in stocks:
-    # stock_code = stock.find_element(By.CSS_SELECTOR, 'css_selector_for_stock_code').text
-    # stock_name = stock.find_element(By.CSS_SELECTOR, 'css_selector_for_stock_name').text
+for row in rows:
+    stock_code = row.find_element(By.CSS_SELECTOR, 'td.code').text
+    stock_name = row.find_element(By.CSS_SELECTOR, 'td.name').text
     stock_data.append([stock_code, stock_name])
 
-with open('hk_stocks_selenium.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
+with open('hk_stocks.csv', 'w', newline='', encoding='UTF8') as f:
+    writer = csv.writer(f)
     writer.writerow(['Stock Code', 'Name'])
     writer.writerows(stock_data)
 
--- readME.md
+++ readME.md
@@ -1,2 +1,21 @@
 This is a folder for a mini project used for web scrapping informations on HKEX for full list of listed Hong Kong Stocks.
 It is aimed to be used in another project which is not shown here.
+
+
+
+
+
+
+To set up and run the project, follow these steps:
+
+pip install selenium
+
+Download geckodriver : https://github.com/mozilla/geckodriver/releases
+
+Extract it
+
+Move to Download folder
+
+mv geckodriver /usr/local/bin/
+
+pip install selenium
