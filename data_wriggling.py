b5bbaba19a50fe30607dce51d1914339fc03d82d
Ken <Ken>
2024-02-15 14:58:10 +0800

    finished

--- data_wriggling.py
+++ data_wriggling.py
@@ -1,3 +1,9 @@
 import pandas as pd
 
 data = pd.read_csv('hk_stocks.csv')
+
+data['Stock Code'] = pd.to_numeric(data['Stock Code'], errors='coerce', downcast='integer')
+
+data_sorted = data.sort_values(by='Stock Code', ascending=True).reset_index(drop=True)
+
+data_sorted.to_csv('hk_stocks_sorted.csv', index=False)
